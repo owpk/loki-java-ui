230585d941614e3a6313e8d3fdbff1b575c0fd2d
Vyacheslav <owpk@MacBook-Air-Nguen.local>
2026-04-17 03:09:23 +0800

    fix web

--- log-client/src/main/java/owpk/jloki/web/LogController.java
+++ log-client/src/main/java/owpk/jloki/web/LogController.java
@@ -2,20 +2,14 @@ package owpk.jloki.web;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import owpk.jloki.core.LokiTemplate;
-import owpk.jloki.core.api.LokiQuerySupported;
-import owpk.jloki.core.dsl.LokiQueryRangeRequest;
-import owpk.jloki.core.model.LogFilterStreamRequest;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import owpk.jloki.core.LokiTemplate;
+import owpk.jloki.core.model.LogFilterStreamRequest;
 import reactor.core.publisher.Flux;
 
 @RestController
@@ -26,20 +20,21 @@ public class LogController {
 
     private final LokiTemplate lokiTemplate;
 
-    @PostMapping("/loki/range_request")
-    public Flux<Object> lokiRangeRequest(@RequestBody String entity) {
-        var req = LokiQueryRangeRequest.builder().build();
-        return lokiTemplate.rangeRequest(req);
-    }
+    // @PostMapping("/loki/range_request")
+    // public Flux<Object> lokiRangeRequest(@RequestBody String entity) {
+    //     var req = LokiQueryRangeRequest.builder().build();
+    //     return lokiTemplate.rangeRequest(req);
+    // }
     
-    @GetMapping(value = "/{app}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Object> streamLogs(
-            @PathVariable String app,
             @RequestParam(required = false) LogFilterStreamRequest filter,
             @RequestParam(required = false, defaultValue = "2") int delaySec) {
         
         if (filter == null)
             filter = new LogFilterStreamRequest();
-        return lokiTemplate.tailLogsStream(null, null).map(it -> it.);
+        return lokiTemplate.tailLogsStream(null, null)
+            .flatMapIterable(it -> it.streams())
+            .map(it -> it.stream());
     }
 }
